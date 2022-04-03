6ca22c5097205bc7759c1320fc50083673aba2cc
Pruthvi Raj <78634459+PruthviRaj0009@users.noreply.github.com>
2022-04-03 21:11:57 +0530

    Create Weatherforecast.py

---
+++ Weatherforecast.py
@@ -0,0 +1,43 @@
+'''
+    CAUTION:
+    run this command first before running the program:
+    
+    " pip install requests "
+    
+    This command imports/install a specific version of the Requests module into a Virtualenv environment for Python 2 or Venv for Python 3 projects.
+    Please don't use the same Api key.
+    API key is available on https://openweathermap.org/api subscribe and get it for free.
+'''
+
+import requests
+#import os
+from datetime import datetime
+
+api_key = '87d845b0b6cf29baa1a73cc34b067a95'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+#printing output
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
+
+with open('weather.txt','w') as f:
+    f.write(api_link.text)
